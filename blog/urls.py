cab3a455c25d4f4d31fcc81dfee395b1f05d595b
Malik-9032 <54151719+Malik-9032@users.noreply.github.com>
2021-12-04 16:41:55 +0500

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls.conf import path
+from .views import PostList, PostDetail, UserDetail, UserList
+
+
+urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
+    path('',PostList.as_view()),
+]
